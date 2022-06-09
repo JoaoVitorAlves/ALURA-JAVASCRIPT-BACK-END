8a57f2a61fa21ce89f463f66a44ce22306647db1
João Vitor Alves de Oliveira <jv.joaovitor.oliveira@gmail.com>
2022-06-09 09:50:22 -0300

    primeiro commit

---
+++ JavaScriptBack-End-Alura/type-boolean.js
@@ -0,0 +1,14 @@
+//Boolean
+//true = verdadeiro, false = falso.
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+let cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero); //
+
+const testo1 = "Alura";
+
+const testo2 = "alura";
+
+console.log(testo1 === testo2); 
---
+++ JavaScriptBack-End-Alura/type-string.js
@@ -0,0 +1,18 @@
+//Tipo string //
+
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura456";
+const stringDeNumeros = "34567";
+
+const citacao = 'O meu nome é: ';
+const meuNome = "Leonardo";
+console.log(citacao);
+
+// Concatenação (+) exemplo:
+
+console.log(citacao + meuNome);
+
+
+//template string OU template literal 
+
