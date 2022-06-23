6ca955d419d2b6796a1e79116e9bd6955a49dcbc
João Vitor Alves de Oliveira <jv.joaovitor.oliveira@gmail.com>
2022-06-23 17:59:26 -0300

    funcoes

--- JavaScriptBack-End-Alura/funcoes.js
+++ JavaScriptBack-End-Alura/funcoes.js
@@ -4,8 +4,23 @@
 //console.log(x);
 //x = "oi";
 
+// Declaração de Função
 
-function ImprimirTexto(texto) {
-    console.log(texto);
+// 1) declara a função
+function imprimirTexto(texto) {
+    console.log(texto)
 }
 
+// 2) executa a função (1 ou + vezes)
+
+//imprimirTexto("João é o melhor do mundo");
+imprimirTexto(soma());
+
+
+// três formas de escrever funções
+
+function soma() {
+    //Outros codigos
+    //varios console.log()
+    return 2 + 2;
+}
