32c95377b3edd2b84ad8d365d4f0f583a2cc61af
João Vitor Alves de Oliveira <jv.joaovitor.oliveira@gmail.com>
2022-08-25 10:13:03 -0300

    adicionar campos

---
+++ objetos/adicionar-campos.js
@@ -0,0 +1,20 @@
+// Adicionando campos
+// Adicionar informações importantes que ainda estão faltando, com telefone
+//e também seus valores.
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "12345678912",
+    email: "andre@email.com"
+}
+
+console.log(cliente);
+//Atribuiu o numero a lista cliente
+cliente.fone = "9242525252";
+
+console.log(cliente);
+
+//Substituiu o numero.
+cliente.fone = "875854554";
+
+console.log(cliente);
