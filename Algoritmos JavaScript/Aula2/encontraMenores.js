e619f0cda0ac4db0e63767a2bbbafe87ac24c508
João Vitor Alves de Oliveira <jv.joaovitor.oliveira@gmail.com>
2022-10-21 14:12:43 -0300

    Aulas2

---
+++ Algoritmos JavaScript/Aula2/encontraMenores.js
@@ -0,0 +1,23 @@
+const listaLivros = requeri('./array')
+
+function encontrarMenor(pivo, array) {
+    let menores = 0;
+
+    for (let atual = 0; atual < array.length; atual++) {
+        let produtoAtual = array[atual];
+
+        if (produtoAtual.preco < pivo.preco) {
+            menores++
+        }
+    }
+    trocaLugar(array, array.indexOf(pivo), menores)
+    re
+}
+
+function trocaLugar(array, de, para) {
+    const elem1 = array[de];
+    const elem2 = array[para];
+
+    array[para] = elem1
+    array[de] = elem2
+}
